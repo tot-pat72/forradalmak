7ea00eb4043a8459a74f6be5b39b31c52c4cbf8c
Patrik <patsboy06@gmail.com>
2025-04-09 19:29:04 +0200

    tablazat letrehozasa oop

--- oop/area.js
+++ oop/area.js
@@ -1,4 +1,12 @@
 class Area{ //Area osztály létrehozása
+    #div //privát változó létrehozása
+
+    /**
+     * @returns {HTMLDivElement}
+     */
+    get div(){ //get létrehozása, hogy el lehessen érni a divet
+        return this.#div //Visszatérés a divvel
+    }
     /**
      * 
      * @param {string} className
@@ -10,8 +18,37 @@ class Area{ //Area osztály létrehozása
             containerDiv.className = "containeroop"; //className adása a div elemnek
             document.body.appendChild(containerDiv); //containerdiv hozzáadása a bodyhoz
         }
-        const div = document.createElement("div"); //div elem létrehozása
-        div.className = className; //className adása a div elemnek
-        containerDiv.appendChild(div); //div hozzáadása a containerdivhez
+        this.#div = document.createElement("div"); //új privát div elem létrehozása 
+        this.#div.className = className; //className adása a div elemnek
+        containerDiv.appendChild(this.#div); //div elem hozzáadása a containerdivhez
+    }
+}
+
+class Table extends Area{ //Table osztály létrehozása, ami az Area leszármazottja
+    /**
+     * 
+     * @param {string} cssClass 
+     */
+    constructor(cssClass){ //constructor létrehozása aminek egy bemeneti paramétere van
+        super(cssClass); //Area osztály constructorának meghívása
+
+        const table = document.createElement('table'); //table elem létrehozása és eltárolása egy változóba
+        this.div.appendChild(table); //hozzáadás az Area által létre hozoztt divhez
+        
+        const head = document.createElement('thead'); //thead elem létrehozása és eltárolása egy változóba
+        table.appendChild(head); //head hozzáadása a tableelementhez
+        
+        const headrow =  document.createElement('tr'); //tr elem létrehozása és eltárolása egy változóba
+        head.appendChild(headrow) //headrow hozzáadása a headhez
+        
+        const headcell = ['forradalom', 'évszám', 'sikeres']; //tömb a fejléc tartalmával
+        for(const cell of headcell){ //headcell tömb bejárása
+            const thcell = document.createElement('th'); //th elem létrehozása és eltárolása egy változóba
+            thcell.innerText = cell; //thcell tartalma a cellában lévő elem lesz
+            headrow.appendChild(thcell); //thcell hozzáadása a headrowhoz
+        }
+        
+        const tbody = document.createElement('tbody'); //tbody elem létrehozása és eltárolása egy változóba
+        table.appendChild(tbody); //tbody hozzáadása a tableelementhez
     }
 }
--- oop/main.js
+++ oop/main.js
@@ -1,5 +1,5 @@
 const separator = document.createElement('hr'); // hogy a html-en egyszeruen megtalalhato legyen az elvalaszto oop es sima kozott
 document.body.appendChild(separator); //separator hozzáadása a bodyhoz
 
-const table = new Area("table"); //Area osztály példányosítása, új objektum létrehozása a table classal
+const table = new Table("table"); //Table osztály példányosítása, új objektum létrehozása a table classal
 const form = new Area("form"); //Area osztály példányosítása, új objektum létrehozása a form classal
