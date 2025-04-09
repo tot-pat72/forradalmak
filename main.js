ad277357ea9eaaeac049b2a41a14bef60dec5bdb
Patrik <patsboy06@gmail.com>
2025-04-09 18:11:29 +0200

    tablazat hozzadasa sima

--- main.js
+++ main.js
@@ -12,6 +12,26 @@ const containerDiv = makeDiv("container"); //containerdiv létrehozása, aminek 
 document.body.appendChild(containerDiv); //containerdiv hozzáadása a bodyhoz
 
 const tableDiv = makeDiv("table"); //tablediv létrehozása, aminek a table lesz a classa
+
+const tableelement = document.createElement('table'); //table elem létrehozása és eltárolása egy változóba
+tableDiv.appendChild(tableelement); //tableelement hozzáadása a tableDivhez
+
+const head = document.createElement('thead'); //thead elem létrehozása és eltárolása egy változóba
+tableelement.appendChild(head); //head hozzáadása a tableelementhez
+
+const headrow =  document.createElement('tr'); //tr elem létrehozása és eltárolása egy változóba
+head.appendChild(headrow) //headrow hozzáadása a headhez
+
+const headcell = ['forradalom', 'évszám', 'sikeres']; //tömb a fejléc tartalmával
+for(const cell of headcell){ //headcell tömb bejárása
+    const thcell = document.createElement('th'); //th elem létrehozása és eltárolása egy változóba
+    thcell.innerText = cell; //thcell tartalma a cellában lévő elem lesz
+    headrow.appendChild(thcell); //thcell hozzáadása a headrowhoz
+}
+
+const tbody = document.createElement('tbody'); //tbody elem létrehozása és eltárolása egy változóba
+tableelement.appendChild(tbody); //tbody hozzáadása a tableelementhez
+
 containerDiv.appendChild(tableDiv); //tablediv hozzáadása a containerdivhez
 
 const formDiv = makeDiv("form"); //formdiv létrehozása, aminek a form lesz a classa
